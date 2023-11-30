b1bdd96b33d6b0312eaff6e13e182544bae328dd
joanmrt_ <jmartinezs@cifpfbmoll.eu>
2023-11-30 20:50:52 +0100

    Version Final

--- app/src/main/java/com/example/examen_1/MainActivity.java
+++ app/src/main/java/com/example/examen_1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.examen_1;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +14,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FragmentList fragment_list = new FragmentList();
+        FragmentList fragment_list_land = new FragmentList();
+        FragmentInfo fragment_info = new FragmentInfo();
 
-        getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainerView, fragment_list).commit();
+
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainerViewLand, fragment_list_land).commit();
+        }
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainerView, fragment_list).commit();
+        }
     }
 }
